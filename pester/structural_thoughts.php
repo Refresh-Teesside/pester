4395e831a5be557cc07b2b0a10f67c8f91cd4064
Matt Kirwan <matthewkirwan@gmail.com>
2013-01-25 14:24:49 +0100

    Thoughts

--- pester/structural_thoughts.php
+++ pester/structural_thoughts.php
@@ -1,1 +1,35 @@
-structural_thoughts.php
+<?php
+
+/* Pester
+Gets a list of all twitter users via eventbrite
+Gets all twitter handles for all registered users to the next event
+Diffs
+Genererates a direct public tweet
+Sends to a random handle
+
+Data:
+> Event
+	*date
+	*countdown
+> Registered users
+	*twitter handles
+> All users
+	*twitter handles
+> Questions
+> Intro
+	* Eg: 'Hi'
+	*maybe time based?
+> Scheduling
+	*how often?
+		*Bufferapp API?
+> Link
+	*to event
+
+Prerequisites:
+
+> Each scheduled tweet should use the latest data (to ensure somebody hasn't registered to the event since).
+> Possibly a storage
+
+**/
+
+
